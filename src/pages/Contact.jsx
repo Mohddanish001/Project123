8bb1655b9615728d90d654801abc0a927e5a30f4
Danish Khan <danishsalmani45434500@gmail.com>
2025-01-29 17:12:51 +0530

    latest

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -8,6 +8,7 @@ export const Contact = () => {
     email: "",
     contact: "",
     requirement: "",
+    Type:"1",
   });
   const [success, setSuccess] = useState(null); 
   const [loading, setLoading] = useState(false);
